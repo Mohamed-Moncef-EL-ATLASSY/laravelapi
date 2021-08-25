4b285a67da69674b05f898371d3f68777237425d
Mohamed Moncef EL ATLASSY <mohamed.moncef.elatlassy@gmail.com>
2021-08-25 15:55:17 +0100

    no need for a view, right?

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
