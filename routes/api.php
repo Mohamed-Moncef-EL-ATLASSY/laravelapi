5b2c7ed6775b02853efc00d377d256b5a95d0383
Mohamed Moncef EL ATLASSY <mohamed.moncef.elatlassy@gmail.com>
2021-08-25 16:51:29 +0100

    making public and protected routes

--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,35 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Public routes
-Route::resource('products', ProductController::class);
+
+////Get one product
+Route::get('/products/{$id}', [ProductController::class, 'show']);
+
+////Get all products
+Route::get('/products', [ProductController::class, 'index']);
+
+////Search by product id
+Route::get('/products/searchById/{id}', [ProductController::class, 'searchByProductId']);
+
+////Search by product name
+Route::get('/products/searchByName/{productName}', [ProductController::class, 'searchByProductName']);
+
 
 //Protected routes
 Route::group (['middleware' => ['auth:sanctum']] , function () {
 
-    // Search by product id
-    Route::get('/products/searchById/{id}', [ProductController::class, 'searchByProductId']);
-
-    // Search by product name
-    Route::get('/products/searchByName/{productName}', [ProductController::class, 'searchByProductName']);
+    //Create a product
+    Route::post('/products', [ProductController::class, 'store']);
 
 });
+
+
+
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+
+// Route::resource('products', ProductController::class);
